9a348837e6e9a79a1bdc65f7c44307c3aa37eed5
chathuwadev <chathuwadev94@gmail.com>
2024-07-12 00:04:49 +0530

    Squashed commit of the following:
    commit 38d69888d1087a4f0c11b61c4b4316eee32b5281
    Author: chathuwadev <chathuwadev94@gmail.com>
    Date:   Thu Jul 11 23:49:54 2024 +0530
    
        Feat: Added comment line, commit message f2
    
    commit 6c1488a6cc73113510bf96e4cfcabe9d2677a8cf
    Author: chathuwadev <chathuwadev94@gmail.com>
    Date:   Thu Jul 11 23:34:05 2024 +0530
    
        initial commit to feature branch f1

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // first commit m1
+<<<<<<< HEAD
 // second commit m2
 
 // first commit to feture brance f1
 
 // second commit to feature branch f2
+=======
+
+// first commit to feture brance f1
+
+// second commit to feature branch f2
+>>>>>>> feature01
