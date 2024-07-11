b9c69938955fe82bad1e93a6b7eacef072646e8b
chathuwadev <chathuwadev94@gmail.com>
2024-07-12 00:05:00 +0530

    dsd

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 // first commit m1
-<<<<<<< HEAD
 // second commit m2
 
 // first commit to feture brance f1
 
 // second commit to feature branch f2
-=======
-
-// first commit to feture brance f1
-
-// second commit to feature branch f2
->>>>>>> feature01
